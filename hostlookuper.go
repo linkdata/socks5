a8072769456fe02e699d0debe9c371beb83bf769
Johan Lindh <johan@linkdata.se>
2025-03-11 08:37:16 +0100

    docs

--- hostlookuper.go
+++ hostlookuper.go
@@ -2,6 +2,7 @@ package socks5
 
 import "context"
 
+// HostLookuper is the signature of net.DefaultResolver.LookupHost
 type HostLookuper interface {
 	LookupHost(ctx context.Context, host string) (addrs []string, err error)
 }
--- logger.go
+++ logger.go
@@ -1,5 +1,6 @@
 package socks5
 
+// Logger is an abstraction of log/slog.
 type Logger interface {
 	Info(msg string, keyvaluepairs ...any)
 	Error(msg string, keyvaluepairs ...any)
