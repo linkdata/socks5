9514781d84813d86584b93b6551e50c6a684c49e
Johan Lindh <johan@linkdata.se>
2025-03-11 08:17:39 +0100

    docs

--- server/credentialstore.go
+++ server/credentialstore.go
@@ -2,14 +2,14 @@ package server
 
 // CredentialStore is used to support user/pass authentication optional network address filtering.
 type CredentialStore interface {
-	Socks5ValidateCredentials(user, password, address string) bool
+	Socks5ValidateCredentials(username, password, address string) bool
 }
 
 // StaticCredentials enables using a map directly as a credential store
 type StaticCredentials map[string]string
 
 // Socks5ValidateCredentials implement interface CredentialStore
-func (s StaticCredentials) Socks5ValidateCredentials(user, password, _ string) bool {
-	pass, ok := s[user]
+func (s StaticCredentials) Socks5ValidateCredentials(username, password, _ string) bool {
+	pass, ok := s[username]
 	return ok && password == pass
 }
